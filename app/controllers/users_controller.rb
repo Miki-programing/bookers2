8ce21ba3ee76fed14e8a0e7174b1320d5e287bc6
Miki-programing <s911m9991@gmail.com>
2021-08-26 05:34:37 +0000

    [Finish] 4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,36 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
+    @book = Book.new
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
   end
-  
+
   def edit
+    if User.find(params[:id]).id == current_user.id
+      @user = User.find(params[:id])
+    else
+      redirect_to user_path(current_user)
+    end
   end
-  
+
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice:'You have updated user successfully.'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
